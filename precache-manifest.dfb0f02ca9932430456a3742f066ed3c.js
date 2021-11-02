5c9fca28bab558daafcfcaf16e1bc25cba20f7f4
hashmesan <hashmesan@gmail.com>
2021-11-02 12:33:00 -0400

    Updates

---
+++ precache-manifest.dfb0f02ca9932430456a3742f066ed3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "16b5fc5851417ee196de",
+    "url": "/static/js/main.16b5fc58.chunk.js"
+  },
+  {
+    "revision": "0d2b268d2ac7acff6404",
+    "url": "/static/js/1.0d2b268d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "906078994f7221a25b900f47425b3ecc",
+    "url": "/static/media/erc20.90607899.svg"
+  },
+  {
+    "revision": "e37299a879ceb5a23000d8a2bb10bc46",
+    "url": "/static/media/xdai.e37299a8.png"
+  },
+  {
+    "revision": "8a3d5d5b0e66212138a9a1d316b451c6",
+    "url": "/index.html"
+  }
+];
